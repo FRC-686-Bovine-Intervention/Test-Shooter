e581878a770f57fb0b9ace3094014b52ede19b3a
FRC Team 686, Bovine Intervention <firstteam686@gmail.com>
2019-11-15 17:40:39 -0500

    Initial commit

---
+++ Test-Shooter/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,92 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.ParamEnum;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  private TalonSRX ShooterMaster = new TalonSRX(1);
+  private double power;
+
+  @Override 
+  public void robotInit() {
+    ShooterMaster.setInverted(true);
+    enableMotors(false);
+
+  }
+
+  @Override
+  public void robotPeriodic() {
+    power = SmartDashboard.getNumber("Shooter Power", 0);
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+    enableMotors(true);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  public void disableInit() {
+    enableMotors(false);
+  }
+
+  private void enableMotors(boolean on) {
+    NeutralMode mode;
+    if (on) {
+      mode = NeutralMode.Brake;
+    }
+    else {
+      mode = NeutralMode.Coast;
+    }
+
+      ShooterMaster.setNeutralMode(mode);
+
+  
+  }
+
+}
